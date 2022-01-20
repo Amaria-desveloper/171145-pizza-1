25f8248fd2a8985aae846d763e1a008838527357
Maria Aleksandrova <mail@aleksandrovamaria.ru>
2022-01-21 00:44:55 +0300

    add: index-страница вёрстка с данными из статики

---
+++ src/frontend/src/common/helpers.js
@@ -0,0 +1,68 @@
+export const SIZES = {
+  1: {
+    value: "small",
+  },
+  2: {
+    value: "normal",
+  },
+  3: {
+    value: "big",
+  },
+};
+
+export const DOUGH = {
+  1: {
+    value: "light",
+  },
+  2: {
+    value: "large",
+  },
+};
+
+export const INGREDIENTS = {
+  1: {
+    value: "mushrooms",
+  },
+  2: {
+    value: "cheddar",
+  },
+  3: {
+    value: "salami",
+  },
+  4: {
+    value: "ham",
+  },
+  5: {
+    value: "ananas",
+  },
+  6: {
+    value: "bacon",
+  },
+  7: {
+    value: "onion",
+  },
+  8: {
+    value: "chile",
+  },
+  9: {
+    value: "jalapeno",
+  },
+  10: {
+    value: "olives",
+  },
+  11: {
+    value: "tomatoes",
+  },
+  12: {
+    value: "salmon",
+  },
+  13: {
+    value: "mozzarella",
+  },
+  14: {
+    value: "parmesan",
+  },
+  15: {
+    value: "blue_cheese",
+  },
+};
